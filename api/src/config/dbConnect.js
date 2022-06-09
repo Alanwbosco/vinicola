e49b4525e702067599f394fe58cda587d9498623
Alanwbosco <79118564+Alanwbosco@users.noreply.github.com>
2022-06-09 00:23:06 -0300

    commit de algumas configurações faltantes

--- api/src/config/dbConnect.js
+++ api/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://rodrigo:rodrigo@alura.rb9pwod.mongodb.net/alura");
+mongoose.connect("mongodb+srv://vinicola:vinicola@cluster0.kwqiz.mongodb.net/?retryWrites=true&w=majority");
 
 
 let db = mongoose.connection;
